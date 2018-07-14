1ec869b199f1f7854baeabbb163fea09b339fe3b
zhouzhangqi <1978892854@qq.com>
2018-07-15 01:38:27 +0800

    add cartoon

---
+++ Cartoon.java
@@ -0,0 +1,23 @@
+package com.zzq;
+
+class Art{
+    Art(){
+        System.out.println("Art constructor");
+    }
+}
+class Drawing extends Art{
+    Drawing(){
+        System.out.println("Drawing constructor");
+    }
+}
+
+
+public class Cartoon extends Drawing {
+//    Cartoon(){
+//        System.out.println("Cartoon constructor");
+//    }
+
+    public static void main(String[] args) {
+        Cartoon x = new Cartoon();
+    }
+}
